03ea90cf630b12c53ac83399113e814dd116d3da
lei.chang <540739522@qq.com>
2018-03-16 09:48:58 +0800

    Initial commit

---
+++ src/me/box/app/elevator/Run.java
@@ -0,0 +1,13 @@
+package me.box.app.elevator;
+
+/**
+ * Created by box on 2018/3/16.
+ * <p>
+ * 应用启动入口
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
